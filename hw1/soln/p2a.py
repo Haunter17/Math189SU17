98f950e94ba4dfe75e735e93a652cc11227a4c86
Haunter17 <haunterhacker17@gmail.com>
2017-04-29 18:29:03 -0700

    Data module for p2

--- hw1/soln/p2a.py
+++ hw1/soln/p2a.py
@@ -1,3 +1,4 @@
+import p2_data as data
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -223,20 +224,12 @@ def plot_description(X_train, y_train, X_test, y_test):
 # ====================main driver function=========================
 if __name__ == '__main__':
 	# =============STEP 0: LOADING DATA=================
-	print('==>Loading data...')
-	name_list = ['pix_{}'.format(i + 1) for i in range(784)]
-	name_list = ['label'] + name_list
-	df_train = pd.read_csv('http://pjreddie.com/media/files/mnist_train.csv', \
-		sep=',', engine='python', names = name_list)
-	df_test = pd.read_csv('http://pjreddie.com/media/files/mnist_test.csv', \
-		sep=',', engine='python', names = name_list)
-	print('==>Data loaded succesfully.')
-	X_train = np.array(df_train[:][[col for col in df_train.columns \
-	if col != 'label']]) / 256.
-	y_train = np.array(df_train[:][['label']])
-	X_test = np.array(df_test[:][[col for col in df_test.columns \
-	if col != 'label']]) / 256.
-	y_test = np.array(df_test[:][['label']])
+	df_train = data.df_train
+	df_test = data.df_test
+	X_train = data.X_train
+	y_train = data.y_train
+	X_test = data.X_test
+	y_test = data.y_test
 
 	# =============STEP 1: Logistic regression=================
 	print('==> Step 1: Running logistic regression...')
