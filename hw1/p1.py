7b924adffe83053f396d8f523745ae6d69762d87
hmc-cs-snake <hli@cs.hmc.edu>
2017-05-01 00:22:20 -0700

    Change format and add comment

--- hw1/p1.py
+++ hw1/p1.py
@@ -1,69 +1,130 @@
+"""
+Start file for hw1pr1 of Big Data Summer 2017
+
+The file is seperated into two parts:
+	1) the helper functions
+	2) the main driver.
+
+The helper functions are all functions necessary to finish the problem.
+The main driver will use the helper function you finished to report and print
+out the results you need for the problem.
+
+Before attemping the helper functions, please familiarize with pandas and numpy
+libraries. Tutorials can be found online:
+http://pandas.pydata.org/pandas-docs/stable/tutorials.html
+https://docs.scipy.org/doc/numpy-dev/user/quickstart.html
+
+First, fill in the the code of step 0 in the main driver to load the data, then
+comment out any steps in main driver before you finish the corresponding
+functions for that step.
+
+After finishing the helper functions for each step, you can uncomment
+the code in main driver to check the result.
+
+Note:
+1. When filling out the functions below, remember to
+	1) Let m be the number of samples
+	2) Let n be the number of features
+
+2. Please read the instructions and hints carefully, and use the name of the
+variables we provided, otherwise, the function may not work.
+
+3. Remember to comment out the TODO comment after you finish each part.
+"""
+
+
+#########################################
+#			 Helper Functions	    	#
+#########################################
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-# ===================================================
-# For the following sections:
-# Let m be the number of samples
-# Let n be the number of features
-# ==============HELPER FUNCTIONS============
+#########################
+#		 Part C			#
+#########################
+
 def linreg(X, y, reg=0.0):
-	'''
-		X is matrix with dimension m x (n + 1).
-		y is label with dimension m x 1.
-		reg is the parameter for regularization.
+	"""	This function takes in three arguments:
+			1) X, the data matrix with dimension m x (n + 1)
+			2) y, the label of the data with dimension m x 1
+			3) reg, the parameter for regularization
 
-		Return the optimal weight matrix.
-	'''
-	# Hint: Find the numerical solution for part c
-	# Use np.eye to create identity matrix
-	# Use np.linalg.solve to solve W_opt
+		This function calculates and returns the optimal weight matrix, W_opt.
 
-	# YOUR CODE GOES BELOW
-	# W_opt = ???
+		HINT: Find the numerical solution for part C
+			1) use np.eye to create identity matrix
+			2) use np.linalg.solve to solve for W_opt
+	"""
+	# TODO: solve for W_opt
+	"*** YOUR CODE HERE ***"
+
+
+	"*** END YOUR CODE HERE ***"
 	return W_opt
 
-def predict(W, X):
-	'''
-		W is a weight matrix with bias.
-		X is the data with dimension m x (n + 1).
 
-		Return the predicted label, y_pred.
-	'''
+
+def predict(W, X):
+	"""	This function takes in two arguments:
+			1) W, a weight matrix with bias
+			2) X, the data with dimension m x (n + 1)
+
+		This function calculates and returns the predicted label, y_pred.
+
+		NOTE: You don't need to change this function.
+	"""
 	return X * W
 
-def find_RMSE(W, X, y):
-	'''
-		W is the weight matrix with bias.
-		X is the data with dimension m x (n + 1).
-		y is label with dimension m x 1.
 
-		Return the root mean-squared error.
-	'''
-	# YOUR CODE GOES BELOW
-	# RMSE = ???
+
+def find_RMSE(W, X, y):
+	"""	This function takes in three arguments:
+			1) W, a weight matrix with bias
+			2) X, the data with dimension m x (n + 1)
+			3) y, the label of the data with dimension m x 1
+
+		This function calculates and returns the root mean-squared error, RMSE
+	"""
+	# TODO: Solve for the root mean-squared error, RMSE
+	"*** YOUR CODE HERE ***"
+
+
+	"*** END YOUR CODE HERE ***"
 	return RMSE
 
-def RMSE_vs_lambda(X_train, y_train, X_val, y_val):
-	'''
-		X is the data with dimension m x (n + 1).
-		y is the label with dimension m x 1.
 
-		Genearte a plot of RMSE vs lambda.
-		Return the regularization parameter that minimizes RMSE.
-	'''
+
+def RMSE_vs_lambda(X_train, y_train, X_val, y_val):
+	"""	This function takes in four arguments:
+			1) X_train, the training data with dimension m x (n + 1)
+			2) y_train, the label of training data with dimension m x 1
+			3) X_val, the validation data with dimension m x (n + 1)
+			4) y_val, the label of validation data with dimension m x 1
+
+		This function generates a plot of RMSE vs lambda and returns the
+		regularization parameter that minimizes RMSE, reg_opt.
+
+		HINT: get a list of RMSE following the steps below:
+			1) Constuct reg_list, a list of regularization parameters with
+			   random uniform sampling
+			2) Generate w_list, a list of W_opt's according to regularization
+			   parameters generated above
+			3) Generate, RMSE_list, a list of RMSE according to reg_list
+	"""
+	# TODO: Generate a list of RMSE, RESE_list
+	RMSE_list = []
+	reg_list = []
+	W_list = []
+	"*** YOUR CODE HERE ***"
+
+
+	"*** END YOUR CODE HERE ***"
+
 	# Set up plot style
 	plt.style.use('ggplot')
-
-	RMSE_list = []
-	# Construct a list of regularization parameters with random uniform sampling
-	# Then, generate a list of W_opt's according to these parameters
-	# Finally, generate a list of RMSE according to reg_list
-	# YOUR CODE GOES BELOW
-	# reg_list = ???
-	# W_list = ???
-	# RMSE_list = ???
 
 	# Plot RMSE vs lambda
 	RMSE_vs_lambda_plot, = plt.plot(reg_list, RMSE_list)
@@ -75,27 +136,42 @@ def RMSE_vs_lambda(X_train, y_train, X_val, y_val):
 	plt.close()
 	print('==> Plotting completed.')
 
-	# Find the regularization value that minimizes RMSE
-	# YOUR CODE GOES BELOW
-	# reg_opt = ???
+	# TODO: Find reg_opt, the regularization value that minimizes RMSE
+	"*** YOUR CODE HERE ***"
+
+
+	"*** END YOUR CODE HERE ***"
 	return reg_opt
 
+
+
 def norm_vs_lambda(X_train, y_train, X_val, y_val):
-	'''
-		X is the data with dimension m x (n + 1).
-		y is the label with dimension m x 1.
+	"""	This function takes in four arguments:
+			1) X_train, the training data with dimension m x (n + 1)
+			2) y_train, the label of training data with dimension m x 1
+			3) X_val, the validation data with dimension m x (n + 1)
+			4) y_val, the label of validation data with dimension m x 1
 
-		Genearte a plot of norm of the weights vs lambda.
-	'''
-	# You may reuse the code for RMSE_vs_lambda
-	# to generate the list of weights and regularization parameters
-	# YOUR CODE GOES BELOW
-	# reg_list = ???
-	# W_list = ???
+		This function generates a plot of norm of the weights vs lambda.
 
-	# Calculate the norm of each weight
-	# YOUR CODE GOES BELOW
-	# norm_list = ???
+		HINT:
+			1) You may reuse the code from RMSE_vs_lambda to generate
+			   w_list, the list of weights, and reg_list, the list of
+			   regularization parameters
+			2) Then generate norm_list, a list of norm by calculating the
+			   norm of each weight
+	"""
+	# TODO: Generate a list of norm, norm_list
+	reg_list = []
+	W_list = []
+	norm_list = []
+	"*** YOUR CODE HERE ***"
+
+
+	"*** END YOUR CODE HERE ***"
+
+	# Set up plot style
+	plt.style.use('ggplot')
 
 	# Plot norm vs lambda
 	norm_vs_lambda_plot, = plt.plot(reg_list, norm_list)
@@ -107,68 +183,103 @@ def norm_vs_lambda(X_train, y_train, X_val, y_val):
 	plt.close()
 	print('==> Plotting completed.')
 
-def linreg_no_bias(X, y, reg = 0.0):
-	'''
-		X is matrix with dimension m x n.
-		y is label with dimension m x 1.
-		reg is the parameter for regularization.
 
-		Return the optimal weight and bias separately.
-	'''
+
+
+#########################
+#		 Part D			#
+#########################
+
+def linreg_no_bias(X, y, reg=0.0):
+	"""	This function takes in three arguments:
+			1) X, the data matrix with dimension m x (n + 1)
+			2) y, the label of the data with dimension m x 1
+			3) reg, the parameter for regularization
+
+		This function calculates and returns the optimal weight matrix, W_opt
+		and bias, b_opt seperately
+	"""
 	t_start = time.time()
+
 	# Find the numerical solution in part d
-	# YOUR CODE GOES BELOW
-	# W_opt = ???
-	# b_opt = ???
+	# TODO: Solve for W_opt, and b_opt
+	"*** YOUR CODE HERE ***"
+
+
+	"*** END YOUR CODE HERE ***"
 
 	# Benchmark report
 	t_end = time.time()
 	print('--Time elapsed for training: {t:4.2f} \
 		seconds'.format(t = t_end - t_start))
+
 	return b_opt, W_opt
 
-def grad_descent(X_train, y_train, X_val, y_val, reg = 0.0, \
-	lr_W = 2.5e-12, lr_b = 0.2, max_iter = 150, eps = 1e-6, print_freq = 25):
-	'''
-		X is matrix with dimension m x n.
-		y is label with dimension m x 1.
-		reg is the parameter for regularization.
-		lr_W is the learning rate for weights.
-		lr_b is the learning rate for bias.
-		max_iter is the maximum number of iterations.
-		eps is the threshold of the norm for the gradients.
-		print_freq is the frequency of printing the report.
 
-		Return the optimal weight and bias by gradient descent.
-	'''
+
+
+#########################
+#		 Part E			#
+#########################
+
+
+def grad_descent(X_train, y_train, X_val, y_val, reg=0.0, lr_W=2.5e-12, \
+ 		lr_b=0.2, max_iter=150, eps=1e-6, print_freq=25):
+	"""	This function takes in ten arguments:
+			1) X_train, the training data with dimension m x (n + 1)
+			2) y_train, the label of training data with dimension m x 1
+			3) X_val, the validation data with dimension m x (n + 1)
+			4) y_val, the label of validation data with dimension m x 1
+			5) reg, the parameter for regularization
+			6) lr_W, the learning rate for weights
+			7) lr_b, the learning rate for bias
+			8) max_iter, the maximum number of iterations
+			9) eps, the threshold of the norm for the gradients
+			10) print_freq, the frequency of printing the report
+
+		This function returns W, the optimal weight, and b, the bias by
+		gradient descent.
+	"""
 	m_train, n = X_train.shape
 	m_val = X_val.shape[0]
-	# initialize the weights and bias and their corresponding gradients
-	# YOUR CODE GOES BELOW
-	# W = ???
-	# b = ???
-	# W_grad = ???
-	# b_grad = ???
+
+	# TODO: initialize the weights and bias and their corresponding gradients
+
+	# Please use the variable names: W (weights), W_grad (gradients of W),
+	# b (bias), b_grad (gradients of b)
+	"*** YOUR CODE HERE ***"
+
+
+	"*** END YOUR CODE HERE ***"
+
+	print('==> Running gradient descent...')
+
+	# TODO: run gradient descent algorithm
+
+	# HINT: Run the gradient descent algorithm followed steps below
+	#	1) Calculate the training RMSE and validation RMSE at each iteration,
+	#      and append these values to obj_train and obj_val respectively
+	#	2) Calculate the gradient for W and b as W_grad and b_grad
+	#	3) Upgrade W and b
+	#	4) Keep iterating while the number of iterations is less than the
+	#	   maximum and the gradient is larger than the threshold
 
 	obj_train = []
 	obj_val = []
-	print('==> Running gradient descent...')
 	iter_num = 0
+
 	t_start = time.time()
-	# Running the gradient descent algorithm
-	# First, calculate the training rmse and validation rmse at each iteration
-	# Append these values to obj_train and obj_val respectively
-	# Then, calculate the gradient for W and b as W_grad and b_grad
-	# Upgrade W and b
-	# Keep iterating while the number of iterations is less than the maximum
-	# and the gradient is larger than the threshold
-	# YOUR CODE GOES BELOW
-	# while...
+	"*** YOUR CODE HERE ***"
+
+
+	"*** END YOUR CODE HERE ***"
 
 	# Benchmark report
 	t_end = time.time()
 	print('--Time elapsed for training: {t:4.2f} \
 		seconds'.format(t = t_end - t_start))
+
+
 	# generate convergence plot
 	train_rmse_plot, = plt.plot(range(iter_num), obj_train)
 	plt.setp(train_rmse_plot, color = 'red')
@@ -185,40 +296,61 @@ def grad_descent(X_train, y_train, X_val, y_val, reg = 0.0, \
 
 	return b, W
 
-# *****************************************************************
-# ====================main driver function=========================
-# You should comment out the sections that you have not completed yet
+
+
+
+###########################################
+#	    	Main Driver Function       	  #
+###########################################
+
+# You should comment out the sections that
+# you have not completed yet
+
 if __name__ == '__main__':
-	# Part c
+
 	# =============STEP 0: LOADING DATA=================
 	print('==> Loading data...')
-	train_pct = 2.0 / 3
-	val_pct = 5.0 / 6
+
+	# Read data
 	df = pd.read_csv('https://math189r.github.io/hw/data/online_news_popularity/online_news_popularity.csv', \
 		sep = ', ', engine = 'python')
+
 	# split the data frame by type: training, validation, and test
+	train_pct = 2.0 / 3
+	val_pct = 5.0 / 6
+
 	df['type'] = ''
 	df.loc[:int(train_pct * len(df)), 'type'] = 'train'
 	df.loc[int(train_pct * len(df)) : int(val_pct * len(df)), 'type'] = 'val'
 	df.loc[int(val_pct * len(df)):, 'type'] = 'test'
+
+
 	# extracting columns into training, validation, and test data
+	# Training data
 	X_train = np.array(df[df.type == 'train'][[col for col in df.columns \
 		if col not in ['url', 'shares', 'type']]])
 	y_train = np.log(df[df.type == 'train'].shares).reshape((-1, 1))
+
+	# Validation data
 	X_val = np.array(df[df.type == 'val'][[col for col in df.columns \
 		if col not in ['url', 'shares', 'type']]])
 	y_val = np.log(df[df.type == 'val'].shares).reshape((-1, 1))
+
+	# Test data
 	X_test = np.array(df[df.type == 'test'][[col for col in df.columns \
 		if col not in ['url', 'shares', 'type']]])
 	y_test = np.log(df[df.type == 'test'].shares).reshape((-1, 1))
 
-	# Stack a column of ones to the feature data
-	# Use np.ones / np.ones_like to create a column of ones
-	# Use np.hstack to stack the column to the matrix
-	# YOUR CODE GOES BELOW
-	# X_train = ???
-	# X_val = ???
-	# X_test = ???
+
+	# TODO: Stack a column of ones to the feature data, X_train, X_val and X_test
+
+	# HINT:
+	# 	1) Use np.ones / np.ones_like to create a column of ones
+	#	2) Use np.hstack to stack the column to the matrix
+	"*** YOUR CODE HERE ***"
+
+
+	"*** END YOUR CODE HERE ***"
 
 	# Convert data to matrix
 	X_train = np.matrix(X_train)
@@ -228,45 +360,70 @@ if __name__ == '__main__':
 	X_test = np.matrix(X_test)
 	y_test = np.matrix(y_test)
 
+
+
+	# PART C
 	# =============STEP 1: RMSE vs lambda=================
+	# NOTE: Fill in code in linreg, findRMSE, and RMSE_vs_lambda for this step
+
 	print('==> Step 1: RMSE vs lambda...')
-	# Fill in the code in linreg, findRMSE, and RMSE_vs_lambda
+
+	# find the optimal regularization parameter
 	reg_opt = RMSE_vs_lambda(X_train, y_train, X_val, y_val)
 	print('==> The optimal regularization parameter is {reg: 4.4f}.'.format(\
 		reg = reg_opt))
+
 	# Find the optimal weights and bias for future use in step 3
 	W_with_b_1 = linreg(X_train, y_train, reg = reg_opt)
 	b_opt_1 = W_with_b_1[0]
 	W_opt_1 = W_with_b_1[1: ]
 
+
+
 	# =============STEP 2: Norm vs lambda=================
+	# NOTE: Fill in code in norm_vs_lambda for this step
+
 	print('==> Step 2: RMSE vs lambda...')
-	# Fill in the code in norm_vs_lambda
 	norm_vs_lambda(X_train, y_train, X_val, y_val)
 
-	# Part d
+
+
+	# PART D
 	# =============STEP 3: Linear regression without bias=================
+	# NOTE: Fill in code in linreg_no_bias for this step
+
 	# From here on, we will strip the columns of ones for all data
 	X_train = X_train[:, 1:]
 	X_val = X_val[:, 1:]
 	X_test = X_test[:, 1:]
-	# Fill in the code in linreg_no_bias
+
 	# Compare the result with the one from step 1
 	# The difference in norm should be a small scalar (i.e, 1e-10)
 	print('==> Step 3: Linear regression without bias...')
 	b_opt_2, W_opt_2 = linreg_no_bias(X_train, y_train, reg = reg_opt)
+
+	# difference in bias
 	diff_bias = np.linalg.norm(b_opt_2 - b_opt_1)
 	print('==> Difference in bias is {diff: 4.4E}'.format(diff = diff_bias))
+
+	# difference in weights
 	diff_W = np.linalg.norm(W_opt_2 -W_opt_1)
 	print('==> Difference in weights is {diff: 4.4E}'.format(diff = diff_W))
 
-	# Part e
+
+
+	# PART E
 	# =============STEP 4: Gradient descent=================
-	# Fill in the code in grad_descent
+	# NOTE: Fill in code in grad_descent for this step
+
 	print('==> Step 4: Gradient descent')
 	b_gd, W_gd = grad_descent(X_train, y_train, X_val, y_val, reg = reg_opt)
+
 	# Compare the result from the one from step 1
+	# Difference in bias
 	diff_bias = np.linalg.norm(b_gd - b_opt_1)
 	print('==> Difference in bias is {diff: 4.4E}'.format(diff = diff_bias))
+
+	# difference in weights
 	diff_W = np.linalg.norm(W_gd -W_opt_1)
 	print('==> Difference in weights is {diff: 4.4E}'.format(diff = diff_W))
