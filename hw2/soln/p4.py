6f71b548b2c46a281ef497e85937852484ac3259
Haunter17 <haunterhacker17@gmail.com>
2017-05-12 01:18:06 -0500

    Init version

--- hw2/soln/p4.py
+++ hw2/soln/p4.py
@@ -51,24 +51,23 @@ def predict(W, X):
 	"""
 	return X @ W
 
-def find_MSE(W, X, y):
+def find_cost(X, y, W , reg):
 	"""	This function takes in three arguments:
 			1) W, a weight matrix with bias
 			2) X, the data with dimension m x (n + 1)
 			3) y, the label of the data with dimension m x 1
 
-		This function calculates and returns the mean-squared error, MSE
+		This function calculates and returns the l1 regularized 
+		mean-squared error
 	"""
-	# TODO: Solve for the mean-squared error, MSE
+	# TODO: Solve for l1-regularized mse
 	"*** YOUR CODE HERE ***"
-	y_pred = predict(W, X)
-	diff = y - y_pred
-	m = X.shape[0]
-	MSE = np.linalg.norm(diff, 2) ** 2 / m
+	err = X @ W - y
+	err = float(err.T @ err)
+	return (err + reg * np.abs(W).sum())/len(y)
 	"*** END YOUR CODE HERE ***"
-	return MSE
 
-def grad(X, y, W, reg=0.0):
+def find_grad(X, y, W, reg=0.0):
 	"""	This function takes in four arguments:
 			1) X, the data with dimension m x (n + 1)
 			2) y, the label of the data with dimension m x 1
@@ -81,6 +80,67 @@ def grad(X, y, W, reg=0.0):
 	m = X.shape[0]
 	return X.T @ (X @ W - y) / m
 	"*** END YOUR CODE HERE ***"
+
+def prox(X, gamma):
+	""" This function takes in two arguments:
+			1)  X, a vector
+			2) gamma, a scalar
+		
+		This function thresholds each entry of X with gamma
+		and updates the changes in place.
+	"""
+	"""*** YOUR CODE HERE ***"""
+	X[np.abs(X) <= gamma] = 0.
+	X[X > gamma] -= gamma
+	X[X < -gamma] += gamma
+	""" END YOUR CODE HERE """
+	return X
+
+def grad_lasso(
+	X, y, reg=1e-6, lr=1e-3, eps=1e-5,
+	max_iter=300, batch_size=256, print_freq=1):
+	""" This function takes in the following arguments:
+			1) X, the data with dimension m x (n + 1)
+			2) y, the label of the data with dimension m x 1
+			3) reg, the parameter for regularization
+			4) lr, the learning rate
+			5) eps, the threshold of the norm for the gradients
+			6) max_iter, the maximum number of iterations
+			7) batch_size, the size of each batch for gradient descent
+			8) print_freq, the frequency of printing the report
+		
+		This function returns W, the optimal weight, 
+		by lasso gradient descent.
+	"""
+	m, n = X.shape
+	obj_list = []
+	# initialize the weight and its gradient
+	W = np.linalg.solve(X.T @ X, X.T @ y)
+	W_grad = np.ones_like(W)
+	print('==> Running gradient descent...')
+	iter_num = 0
+	t_start = time.time()
+	"*** YOUR CODE HERE ***"
+	while iter_num < max_iter and np.linalg.norm(W_grad) > eps:
+		# update W
+		ind = np.random.randint(0, m, size=batch_size)
+		W_grad = find_grad(X[ind], y[ind], W, reg=reg)
+		# apply threshold function
+		W = prox(W - lr * W_grad, reg * lr)
+		# calculate MSE
+		cost = find_cost(X[ind], y[ind], W, reg=reg)
+		obj_list.append(cost)
+		if (iter_num + 1) % print_freq == 0:
+			print('-- Iteration{} - training cost {: .4E} - \
+				gradient norm {: .4E}'.format(iter_num + 1, cost, \
+					np.linalg.norm(W_grad)))
+		iter_num += 1
+	"*** END YOUR CODE HERE ***"
+	# Benchmark report
+	t_end = time.time()
+	print('--Time elapsed for training: {t:4.2f} \
+		seconds'.format(t = t_end - t_start))
+	return W
 
 ###########################################
 #	    	Main Driver Function       	  #
@@ -100,3 +160,7 @@ if __name__ == '__main__':
 	X = df[[col for col in df.columns if col not in ['url', 'shares', 'cohort']]]
 	y = np.log(df.shares).values.reshape(-1,1)
 	X = np.hstack((np.ones_like(y), X))
+	# =============STEP 1: LASSO GRADIENT DESCENT=================
+	# NOTE: Fill in code in find_MSE, find_grad, prox and 
+	# grad_lasso for this step
+	W_opt = grad_lasso(X, y)
