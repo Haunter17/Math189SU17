89a8e8da14497c9116cdac6668a0c61f37aa7e0a
Haunter17 <haunterhacker17@gmail.com>
2017-05-21 15:45:54 -0700

    nmf done

--- hw4/soln/nmf_als.py
+++ hw4/soln/nmf_als.py
@@ -36,6 +36,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from nltk.corpus import reuters
 from sklearn.feature_extraction import text
+import time
 
 #########################################
 #			 Helper Functions	    	#
@@ -49,31 +50,62 @@ def nmf_cost(X, W, H):
 		This function calculates and returns the cost defined by
 		|X - WH|^2.
 
-		HINT: 
+		HINT:
+			1) Use X.tocoo() to cast a matrix into sparse matrix
+			2) Use S.row to access the row index of nonzero entries for 
+				a sparse matrix S
+			3) Use S.col to access the column index of nonzero entries for 
+				a sparse matrix S
+			4) Use S.data to access the value of nonzero entries for a 
+				sparse matrix S
 	"""
-	return np.linalg.norm(X - W @ H)
-	# cost = 0.
-	# sparse_X = X.tocoo()
-	# for i, j, x in zip(sparse_X.row, sparse_X.col, sparse_X.data):
-	# 	cost += (x - np.inner(W[i], H[:, j])) ** 2
-	# return cost
+	# TODO: Calculate the cost for nmf algorithm
+	cost = 0.
+	"*** YOUR CODE HERE ***"
+	sparse_X = X.tocoo()
+	for index in range(len(sparse_X.data)):
+		row = sparse_X.row[index]
+		col = sparse_X.col[index]
+		data = sparse_X.data[index]
+		cost += (data - (W[row, :] @ H[:, col]).item(0)) ** 2
+	"*** END YOUR CODE HERE ***"
+	return cost
 
 def nmf(X, k=20, max_iter=100, print_freq=5):
+	"""	This function takes in three arguments:
+			1) X, the data matrix with dimension m x n
+			2) k, the number of latent factors
+			3) max_iter, the maximum number of iterations
+			4) print_freq, the frequency of printing the report
+
+		This function runs the nmf algorithm and returns the following:
+			1) W, a matrix with dimension m x k
+			2) H, a matrix with dimension k x n
+			3) cost_list, a list of costs at each iteration
+	"""
 	m, n = X.shape
 	W = np.abs(np.random.randn(m, k) * 1e-3)
 	H = np.abs(np.random.randn(k, n) * 1e-3)
-
 	cost_list = [nmf_cost(X, W, H)]
+	t_start = time.time()
+	
 	for iter_num in range(max_iter):
+		"*** YOUR CODE HERE ***"
 		H = H * (W.T @ X) / ((W.T @ W) @ H)
 		W = W * (X @ H.T) / (W @ (H @ H.T))
 		cost = nmf_cost(X, W, H)
 		cost_list.append(cost)
+		"*** END YOUR CODE HERE ***"
 		if (iter_num + 1) % print_freq == 0:
 			print('-- Iteration {} - cost: {:.4E}'.format(iter_num + 1, \
 				cost))
 
-	return W, H.T, cost_list
+	# Benchmark report
+	t_end = time.time()
+	print('--Time elapsed for running nmf: {t:4.2f} seconds'.format(\
+			t=t_end - t_start))
+
+	return W, H, cost_list
 
 ###########################################
 #	    	Main Driver Function       	  #
@@ -95,4 +127,30 @@ if __name__ == '__main__':
 	# =============STEP 1: RUNNING NMF=================
 	# NOTE: Fill in code in nmf_cost(), nmf() for this step
 	print('==> Running nmf algorithm on the dataset...')
-	W, H, cost_list = nmf(X, print_freq=1)
+	W, H, cost_list = nmf(X, k=20, print_freq=10)
+	# =============STEP 2: CONVERGENCE PLOT=================
+	print('==> Generating convergence plot...')
+	plt.style.use('ggplot')
+	plt.plot(cost_list)
+	plt.xlabel('iteration')
+	plt.ylabel('cost')
+	plt.title('Reuters NMF Convergence Plot with {} Topics'.format(H.shape[0]))
+	plt.savefig('nmf_cvg.png', format='png')
+	plt.close()
+	# =============STEP 3: FIND MOST FREQUENT WORDS=================
+	print('==> Finding most frequent words for each topic...')
+	num_top_words = 10
+	'''
+		NOTE: This corresponds to the largest values of each row of H
+		HINT:
+				1) Use np.argsort to find the index of the sorted array
+				2) Use np.flip to flip the order of an array
+				3) ind should have the shape of k x num_top_words
+	'''
+	# TODO: Find the index of the most frequent words for each topic
+	"*** YOUR CODE HERE ***"
+	ind = np.flip(np.argsort(H, axis=1), 1)
+	"*** END YOUR CODE HERE ***"
+	top_words = np.array(tfidf.get_feature_names())[ind]
+	for topic_ind in range(H.shape[0]):
+		print('-- topic {}: {}'.format(topic_ind + 1, top_words[topic_ind]))
